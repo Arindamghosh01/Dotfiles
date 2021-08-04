20438a0fa76b1723470e845b0d487934f8ca2dd3
Arindam Ghosh <arindamghosh7878@gmail.com>
2021-08-04 20:13:58 +0530

    init

---
+++ .template/$PROBLEM_NAME.cpp
@@ -0,0 +1,30 @@
+//! -- Author - arindam
+#include <bits/stdc++.h>
+using namespace std;
+
+// ----- Defines -----
+#define fi first
+#define se second
+#define pb push_back
+#define mp make_pair
+#define mt make_tuple
+#define sz(a) ((int)((a).size()))
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define print_arr(a) for(int ez=0;ez<sz(a);ez++) cout<<a[ez]<<" \n"[ez==sz(a)-1];
+typedef double ld;
+typedef long double lld;
+typedef string str;
+constexpr int inf=0x3f3f3f3f;
+
+template <typename T>inline void chmax(T &a,const T b){(a<b)&&(a=b);}
+template <typename T>inline void chmin(T &a,const T b){(b<a)&&(a=b);}
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(nullptr);
+    // cout.precision(10); cout << fixed;
+
+    
+
+    return 0;
+}
